973c9fe42c9f98b39a2ba5aa6bba31ea7a9a97f8
pablomferrara-creator <pablomferrara@gmail.com>
2026-04-27 09:19:56 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    charset: 'utf8'
-  }
-})
+export default defineConfig({ plugins: [react()] })
